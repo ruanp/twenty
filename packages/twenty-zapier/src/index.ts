51056fefd4d67ee0c199549446205bae2def662a
martmull <martmull@hotmail.fr>
2023-12-21 16:38:33 +0100

    Revert "Remove zapier trigger company (#2742)"
    This reverts commit 7b02391b220003eb56a75dc136375482af70ead1.

--- packages/twenty-zapier/src/index.ts
+++ packages/twenty-zapier/src/index.ts
@@ -2,6 +2,7 @@ const { version } = require('../package.json');
 import { version as platformVersion } from 'zapier-platform-core';
 import createRecord from './creates/create_record';
 import findObjects from './triggers/find_objects'
+import company from './triggers/company';
 import authentication from './authentication';
 import 'dotenv/config';
 
@@ -11,6 +12,7 @@ export default {
   authentication: authentication,
   triggers: {
     [findObjects.key]: findObjects,
+    [company.key]: company,
   },
   creates: {
     [createRecord.key]: createRecord,
